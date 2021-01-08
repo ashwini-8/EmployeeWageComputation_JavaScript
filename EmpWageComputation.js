f92e59493e2a6dab3bd899b0bf6a1d07a9367c8f
ashwini-8 <ashwinip309@gmail.com>
2021-01-08 22:58:19 +0530

    Added:UC4 Employee Wage Switch Case

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -4,21 +4,28 @@ function EmployeeWageProgram() {
     let Present = 1;
     //UC2
     const IS_FULL_TIME = 1;
+    const IS_PART_TIME = 2;
     const EMP_RATE_PER_HOUR=20;
-    let empHrs ;
+    let empHrs;
     let empWage;
-    let  empCheck = Math.floor(Math.random() * 10) % 3;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
     //UC3
-    if (empCheck==IS_FULL_TIME) {
-        console.log("employee is full time present ")
-        empHrs = 8
-        empWage = empHrs * EMP_RATE_PER_HOUR
-        console.log("employeeWage ",empWage)
-    } else
-    {   console.log("employee is part time present  ")
-        empHrs = 4
-        empWage = empHrs * EMP_RATE_PER_HOUR
-        console.log("employeeWage ",empWage)
-        }
-     }
+    switch(empCheck){
+        //UC4
+        case  IS_FULL_TIME:
+            console.log("employee is full time ")
+             empHrs = 8
+             empWageDay = empHrs * EMP_RATE_PER_HOUR 
+            console.log("employee Wage for a day ",empWageDay)
+            break;
+        case IS_PART_TIME:
+            console.log("employee is part time ")
+             empHrs = 4
+             empWageDay = empHrs * EMP_RATE_PER_HOUR 
+            console.log("employee Wage for a day ",empWageDay)
+            break;
+        default:
+            console.log("employee is absent ")
+    }
+}
 EmployeeWageProgram();
