24c31df51ab16581b7f7cc448e0d856a47dff3e5
ashwini-8 <ashwinip309@gmail.com>
2021-01-08 22:08:03 +0530

    Added:UC2 Daily Employee Wage

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -3,9 +3,16 @@ function EmployeeWageProgram() {
   
     //UC1
     let Present = 1;
-    let  empCheck = Math.floor(Math.random() * 10) % 3;
+    //UC2
+    const IS_FULL_TIME = 1;
+    const EMP_RATE_PER_HOUR=20;
+    let empHrs = 8;
+    let  empCheck = Math.floor(Math.random() * 10) % 2;
+    
     if (empCheck == Present) {
-        console.log("employee is present ");
+        console.log("employee is present ")
+        let empWage = empHrs * EMP_RATE_PER_HOUR
+        console.log("employeeWage ",empWage);
     } else {
         console.log("employee is absent ");
     }
