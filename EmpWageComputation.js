084a82b386aa7b66129d400e570f4128f70afc29
ashwini-8 <ashwinip309@gmail.com>
2021-01-08 21:54:35 +0530

    Added:UC1 Employee Present or Absent

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -1,4 +1,13 @@
 function EmployeeWageProgram() {
     console.log("Welcome to Employee Wage Computation Program Using JavaScript")
-}
+  
+    //UC1
+    let Present = 1;
+    let  empCheck = Math.floor(Math.random() * 10) % 3;
+    if (empCheck == Present) {
+        console.log("employee is present ");
+    } else {
+        console.log("employee is absent ");
+    }
+  }
 EmployeeWageProgram();
