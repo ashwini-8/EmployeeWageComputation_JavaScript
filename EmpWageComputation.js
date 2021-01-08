583dd98f7c14cccddb83e7a628f8d0be3087a112
ashwini-8 <ashwinip309@gmail.com>
2021-01-08 22:38:05 +0530

    Added:UC2 Daily Employee Wage

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -1,20 +1,24 @@
 function EmployeeWageProgram() {
     console.log("Welcome to Employee Wage Computation Program Using JavaScript")
-  
     //UC1
     let Present = 1;
     //UC2
     const IS_FULL_TIME = 1;
     const EMP_RATE_PER_HOUR=20;
-    let empHrs = 8;
-    let  empCheck = Math.floor(Math.random() * 10) % 2;
-    
-    if (empCheck == Present) {
-        console.log("employee is present ")
-        let empWage = empHrs * EMP_RATE_PER_HOUR
-        console.log("employeeWage ",empWage);
-    } else {
-        console.log("employee is absent ");
-    }
-  }
+    let empHrs ;
+    let empWage;
+    let  empCheck = Math.floor(Math.random() * 10) % 3;
+    //UC3
+    if (empCheck==IS_FULL_TIME) {
+        console.log("employee is full time present ")
+        empHrs = 8
+        empWage = empHrs * EMP_RATE_PER_HOUR
+        console.log("employeeWage ",empWage)
+    } else
+    {   console.log("employee is part time present  ")
+        empHrs = 4
+        empWage = empHrs * EMP_RATE_PER_HOUR
+        console.log("employeeWage ",empWage)
+        }
+     }
 EmployeeWageProgram();
