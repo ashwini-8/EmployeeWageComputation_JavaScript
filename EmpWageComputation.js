f7af4082d4f5611f57fb7188cef46a57fa255d00
ashwini-8 <ashwinip309@gmail.com>
2021-01-08 23:07:53 +0530

    Added:UC5 Employee Wages for a month

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -5,27 +5,30 @@ function EmployeeWageProgram() {
     //UC2
     const IS_FULL_TIME = 1;
     const IS_PART_TIME = 2;
-    const EMP_RATE_PER_HOUR=20;
+    const EMP_RATE_PER_HOUR = 20;
+    const NUM_OF_WORKING_DAYS = 20;
     let empHrs;
     let empWage;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     //UC3
-    switch(empCheck){
-        //UC4
-        case  IS_FULL_TIME:
-            console.log("employee is full time ")
-             empHrs = 8
-             empWageDay = empHrs * EMP_RATE_PER_HOUR 
-            console.log("employee Wage for a day ",empWageDay)
-            break;
-        case IS_PART_TIME:
-            console.log("employee is part time ")
-             empHrs = 4
-             empWageDay = empHrs * EMP_RATE_PER_HOUR 
-            console.log("employee Wage for a day ",empWageDay)
-            break;
-        default:
-            console.log("employee is absent ")
+    for(let i = 0 ; i < NUM_OF_WORKING_DAYS; i++ ){
+        switch(empCheck){
+            //UC4
+            case  IS_FULL_TIME:
+                console.log("employee is full time ")
+                 empHrs = 8
+                 empWageDay = empHrs * EMP_RATE_PER_HOUR 
+                console.log("employee Wage for a day ",empWageDay)
+                break;
+            case IS_PART_TIME:
+                console.log("employee is part time ")
+                 empHrs = 4
+                 empWageDay = empHrs * EMP_RATE_PER_HOUR 
+                console.log("employee Wage for a day ",empWageDay)
+                break;
+            default:
+                console.log("employee is absent ")
+        }
     }
 }
 EmployeeWageProgram();
