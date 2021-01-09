702dc2b57c4be79646af0fe93f2d1c9bc09f427d
ashwini-8 <ashwinip309@gmail.com>
2021-01-09 12:22:36 +0530

    Refactored: daily wage till a condition

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -1,8 +1,6 @@
 function EmployeeWageProgram() {
     console.log("Welcome to Employee Wage Computation Program Using JavaScript")
-    //UC1
     let Present = 1;
-    //UC2
     const IS_FULL_TIME = 1;
     const IS_PART_TIME = 2;
     const EMP_RATE_PER_HOUR = 20;
@@ -10,27 +8,25 @@ function EmployeeWageProgram() {
     const MAX_HRS_IN_MONTH = 100;
     let empHrs = 0;
     let empWage = 0;
-    let totalWorkingDays=0;
-    let totalEmpHrs=0;
-    let empCheck =0;
-   
-    //UC3 
+    let totalWorkingDays = 0;
+    let totalEmpHrs = 0;
+    let empCheck = 0;
+    
     while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS ){
         totalWorkingDays++; 
         empCheck = Math.floor(Math.random() * 10) % 3;
     
         switch(empCheck){
-            //UC4
             case  IS_FULL_TIME:
                 console.log("employee is full time ")
-                 empHrs = 8
-                 empWageDay = empHrs * EMP_RATE_PER_HOUR 
+                empHrs = 8
+                empWageDay = empHrs * EMP_RATE_PER_HOUR 
                 console.log("employee Wage for a day ",empWageDay)
                 break;
             case IS_PART_TIME:
                 console.log("employee is part time ")
-                 empHrs = 4
-                 empWageDay = empHrs * EMP_RATE_PER_HOUR 
+                empHrs = 4
+                empWageDay = empHrs * EMP_RATE_PER_HOUR 
                 console.log("employee Wage for a day ",empWageDay)
                 break;
             default:
