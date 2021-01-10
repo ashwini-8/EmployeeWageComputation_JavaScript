1daffe58a0d80ca87118055d0aa22aeeb0efd6f9
ashwini-8 <ashwinip309@gmail.com>
2021-01-10 22:08:16 +0530

    Refactored: daily wage till a condition and calculated monthly wage

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -11,29 +11,30 @@ function EmployeeWageProgram() {
     let totalWorkingDays = 0;
     let totalEmpHrs = 0;
     let empCheck = 0;
+    let  monthlyWage = 0;
     
-    while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS ){
-        totalWorkingDays++; 
+    for( totalWorkingDays = 0;totalWorkingDays<NUM_OF_WORKING_DAYS;  totalWorkingDays++){
+       
         empCheck = Math.floor(Math.random() * 10) % 3;
     
         switch(empCheck){
             case  IS_FULL_TIME:
                 console.log("employee is full time ")
                 empHrs = 8
-                empWageDay = empHrs * EMP_RATE_PER_HOUR 
-                console.log("employee Wage for a day ",empWageDay)
                 break;
             case IS_PART_TIME:
                 console.log("employee is part time ")
                 empHrs = 4
-                empWageDay = empHrs * EMP_RATE_PER_HOUR 
-                console.log("employee Wage for a day ",empWageDay)
-                break;
+               break;
             default:
                 console.log("employee is absent ")
-                empWageDay = empHrs * EMP_RATE_PER_HOUR
-                console.log("employee Wage for a day ",empWageDay)
         }
-    }
+        if(totalEmpHrs<=MAX_HRS_IN_MONTH ){
+            empWageDay = empHrs * EMP_RATE_PER_HOUR 
+            monthlyWage = monthlyWage + empWageDay;
+            totalEmpHrs = totalEmpHrs+empHrs
+            }
+        }
+    console.log("employee Wage for a Month ",monthlyWage)
 }
 EmployeeWageProgram();
